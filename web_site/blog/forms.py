b5f076f425e08731ffdfe30a9be01f5b47d324b5
roma <trueget33@gmail.com>
2022-09-10 23:15:06 +0300

    прописал форму

--- web_site/blog/forms.py
+++ web_site/blog/forms.py
@@ -5,3 +5,5 @@ from django import forms
 
 class PublishState(forms.Form):
     nick_name = forms.CharField(label='Ваше имя', required=False, max_length=30)
+    img_field = forms.ImageField(label='Выберите изображение для статьи')
+    text_state = forms.CharField(label='Текст статьи', widget=forms.Textarea)
--- web_site/blog/views.py
+++ web_site/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import PublishState
 
 # Create your views here.
 
@@ -6,8 +8,16 @@ def index_page(request):
     return render(request, 'blog/index.html')
 
 
+# def publish_page(request):
+#     if request.method == 'POST':
+#         form = PublishState(request.POST)
+#         if form.is_valid():
+#     return render(request, 'blog/publish.html')
+
 def publish_page(request):
-    return render(request, 'blog/publish.html')
+    publish_stat = PublishState()
+    return render(request, 'blog/publish.html', {'publish_stat': publish_stat})
+
 
 
 def news_page(request):
