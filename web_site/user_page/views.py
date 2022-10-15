d76ead6fd1a46ccb930b928351cff18ea9e4f997
roma <trueget33@gmail.com>
2022-10-15 12:23:47 +0300

    страница пользователя, продолжаю работу

--- web_site/user_page/views.py
+++ web_site/user_page/views.py
@@ -1,3 +1,4 @@
+import profile
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -13,11 +14,14 @@ def update_profile(request):
         form = UserProfileForm(request.POST, request.FILES)
         if form.is_valid():
             user = User.objects.get(username=request.user.username)
-            # if UserProfile.objects.filter
             avatar = form.cleaned_data.get('user_avatar')
             about_user = form.cleaned_data.get('about_me')
-            UserProfile.objects.create(user=user, user_avatar=avatar, about_me=about_user)
-            print('Ваш профиль был успешно обновлен!')
+            if UserProfile.objects.filter(user=user):
+                UserProfile.objects.update(user=user, user_avatar=avatar, about_me=about_user)
+                print('Ваш профиль был успешно обновлен!')
+            else:
+                UserProfile.objects.create(user=user, user_avatar=avatar, about_me=about_user)
+                print('Ваш профиль был успешно добавлен!')
             return render(request, 'blog/user_page.html', {'form': form})
         else:
             print('Форма не валидна')
@@ -25,3 +29,10 @@ def update_profile(request):
     else:
         form = UserProfileForm()
         return render(request, 'blog/user_page.html', {'form': form})
+
+
+def my_profile(request):
+    my_data = UserProfile.objects.get(user=request.user.username)
+    print(my_data)
+    form = {'avatar': my_data.user_avatar, 'info': my_data.about_me}
+    return render(request, 'blog/user_page.html', {'form': form})
